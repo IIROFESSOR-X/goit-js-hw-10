bbdd6e1ffa5b6d2936234b464a9bd75fc3797091
IIROFESSOR-X <IIROFESSOR@gmail.com>
2024-01-19 15:51:14 +0200

    final fix

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -46,13 +46,13 @@ input.addEventListener('focus', () => {
 btnStart.addEventListener('click', onStartTimer);
 
 function onStartTimer() {
-  btnStart.disabled = true;
-  input.disabled = true;
   const timer = setInterval(() => {
+    btnStart.disabled = true;
+    input.disabled = true;
     const selectedDateTime = userSelectedDate.getTime();
     const currentDateTime = new Date().getTime();
-    const different = selectedDateTime - currentDateTime - 1000;
-    const result = convertMs(different);
+    const difference = selectedDateTime - currentDateTime;
+    const result = convertMs(difference);
 
     const { days, hours, minutes, seconds } = result;
 
@@ -61,15 +61,21 @@ function onStartTimer() {
     minsTimer.textContent = pad(minutes);
     secsTimer.textContent = pad(seconds);
 
-    if (different <= 0) {
+    if (difference <= 0) {
       clearInterval(timer);
+      daysTimer.textContent = "00";
+      hoursTimer.textContent = "00";
+      minsTimer.textContent = "00";
+      secsTimer.textContent = "00";
+      alert('Countdown Finished');
     }
-  }, 1000);
+  }, 1000)
 }
 
 function pad(value) {
   return String(value).padStart(2, '0');
 }
+
 
 function convertMs(ms) {
   
@@ -78,24 +84,10 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  
   const days = Math.floor(ms / day);
-  
   const hours = Math.floor((ms % day) / hour);
-  
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
-
-btnStart.disabled = true;
-
-btnStart.addEventListener('click', () => {
-  const selectedDate = datetimePicker.selectedDates[0];
-  const endTime = selectedDate.getTime();
-
-  startButton.disabled = true;
-  startCountdown(endTime);
-});
